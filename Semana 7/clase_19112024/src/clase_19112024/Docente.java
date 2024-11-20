7324a2a3ebf17bce5d73877321c9c4ff3e4c817b
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2024-11-19 18:35:56 -0600

    Clase 19: Jtree

---
+++ Semana 7/clase_19112024/src/clase_19112024/Docente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_19112024;
+
+/**
+ *
+ * @author claudiacortes
+ */
+public class Docente extends Usuario{
+
+    public Docente(String nombre) {
+        super(nombre);
+    }
+    
+}
---
+++ Semana 7/clase_19112024/src/clase_19112024/Usuario.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_19112024;
+
+/**
+ *
+ * @author claudiacortes
+ */
+public class Usuario {
+    protected  String nombre;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + '}';
+    }
+    
+    
+    
+}
