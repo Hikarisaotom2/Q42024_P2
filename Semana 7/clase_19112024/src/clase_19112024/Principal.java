40d7d8b6ae49798aa59d2ce3f5caab3b8c3fca35
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2024-11-20 20:16:21 -0600

    Clase 20 nov: jtree

--- Semana 7/clase_19112024/src/clase_19112024/Principal.java
+++ Semana 7/clase_19112024/src/clase_19112024/Principal.java
@@ -21,25 +21,41 @@ public class Principal extends javax.swing.JFrame {
         // no tenemos elementos visuales creados aun
         initComponents();
         // ya tenemos los elementos visuales creados, pero aun no se muestra la pantalla
+        //limpiar el arbol. 
         DefaultTreeModel modeloArbol = (DefaultTreeModel)jt_usuarios.getModel();
         DefaultMutableTreeNode raiz = (DefaultMutableTreeNode)modeloArbol.getRoot();
         raiz.removeAllChildren();
         modeloArbol.reload();
-        /// un arbol vacio con una raiz 
+        // agregando al arbol las cartegorias de docentes y alumnos
+        agregarCategoriasUsuarios();
         
-        DefaultTreeModel modelo = (DefaultTreeModel) JTree.getModel();
-        DefaultMutableTreeNode nodos = (DefaultMutableTreeNode) modelo.getRoot();
-        DefaultMutableTreeNode nodo1 = new DefaultMutableTreeNode("Nodo 1");
-        DefaultMutableTreeNode subnodo1 = new DefaultMutableTreeNode("Subnodo 1");
-        
-        //agregar subnodo a nodo
-        
-        if (subnodo1.isLeaf()) {
-            nodo1.remove(subnodo1);
-            DefaultMutableTreeNode nvoRaiz = subnodo1;
-        }
-        
-        
+    }
+    
+    /*
+    Este método crea las categorias/ secciones de usuarios (docente y alumno) dentro del arbol. 
+    */
+    private void agregarCategoriasUsuarios(){
+      DefaultTreeModel modeloArbol = (DefaultTreeModel)jt_usuarios.getModel();
+      DefaultMutableTreeNode raiz = (DefaultMutableTreeNode)modeloArbol.getRoot();   
+      
+      // Creando nodos para cada categoria 
+      // Nota: el parametro del constructor es del tipo Object
+      
+      /*
+      Observaciones:
+        1. en este caso, los nodos que acabaos de crear son hojas.
+        2. creamos los nodos, pero estos aun no son parte del arbol. 
+        3. cuaquier modificaciôn al arbol, requiere un reload. 
+      */
+      DefaultMutableTreeNode nodoDocentes = new DefaultMutableTreeNode("Doocentes");
+      DefaultMutableTreeNode nodoAlumnos = new DefaultMutableTreeNode("Alumnos");
+      
+      //Agregar los nodos/ hojas a la raiz 
+      raiz.add(nodoDocentes);
+      raiz.add(nodoAlumnos);
+      
+      // Recargar el modelo para que actualice la vista. 
+      modeloArbol.reload();
     }
 
     /**
@@ -58,9 +74,16 @@ public class Principal extends javax.swing.JFrame {
         txt_nombre = new javax.swing.JTextField();
         jcb_tipoUsuario = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        lbl_usuarioSeleccionado = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jt_usuarios.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jt_usuariosMouseClicked(evt);
+            }
+        });
         jScrollPane2.setViewportView(jt_usuarios);
 
         jLabel1.setText("Nombre");
@@ -76,6 +99,10 @@ public class Principal extends javax.swing.JFrame {
 
         jLabel2.setText("Tipo de usuario");
 
+        jLabel3.setText("Usuario seleccionado: ");
+
+        lbl_usuarioSeleccionado.setText("jLabel4");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -84,20 +111,27 @@ public class Principal extends javax.swing.JFrame {
                 .addGap(62, 62, 62)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(65, 65, 65)
-                        .addComponent(jButton1))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(18, 18, 18)
+                                .addComponent(jcb_tipoUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(37, 37, 37)
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(txt_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(47, 47, 47))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(37, 37, 37)
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(txt_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jcb_tipoUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(47, 47, 47))
+                        .addGap(6, 6, 6)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3)
+                            .addComponent(jButton1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(lbl_usuarioSeleccionado, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -117,28 +151,67 @@ public class Principal extends javax.swing.JFrame {
                             .addComponent(txt_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(26, 26, 26)
                         .addComponent(jButton1)))
-                .addContainerGap(189, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(lbl_usuarioSeleccionado))
+                .addGap(98, 98, 98))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-        // TODO add your handling code here:
+      
         
         String nombre = txt_nombre.getText();
-        
+          DefaultTreeModel modeloArbol = (DefaultTreeModel)jt_usuarios.getModel();
+      DefaultMutableTreeNode raiz = (DefaultMutableTreeNode)modeloArbol.getRoot();   
+      
+        System.out.println("La raiz tiene "+ raiz.getChildCount() +"Hijos");
         if(jcb_tipoUsuario.getSelectedIndex() == 0){ //docente
             Docente docente = new Docente(nombre);
-            System.out.println(docente);
+            
+            DefaultMutableTreeNode nodoDocentes  = (DefaultMutableTreeNode)raiz.getChildAt(0);
+            // agregar el hio al nodo
+            DefaultMutableTreeNode nuevoDocente = new DefaultMutableTreeNode(docente);
+            nodoDocentes.add(nuevoDocente);
+            modeloArbol.reload();
         }else{ //alumno
             Alumno alumno = new Alumno(nombre);
-            System.out.println(alumno);
+            DefaultMutableTreeNode nodoAlumnos = (DefaultMutableTreeNode)raiz.getChildAt(1);
+            DefaultMutableTreeNode nuevoAlumno = new DefaultMutableTreeNode(alumno);
+            nodoAlumnos.add(nuevoAlumno);
+            modeloArbol.reload();
         }
         
         JOptionPane.showMessageDialog(null, "Usuario agregado exitosamente'");
         
     }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jt_usuariosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jt_usuariosMouseClicked
+        // TODO add your handling code here:
+        
+        // selecction path -> 
+        System.out.println("Selecction path ");
+        System.out.println(jt_usuarios.getSelectionPath());
+        
+        Object seleccionado = jt_usuarios.getSelectionPath().getLastPathComponent();
+        DefaultMutableTreeNode nodoSeleccionado = (DefaultMutableTreeNode) seleccionado;
+        
+        Object objetoSeleccionado = nodoSeleccionado.getUserObject();
+        
+        if(objetoSeleccionado instanceof Docente){
+            Docente doc = (Docente)objetoSeleccionado;
+            lbl_usuarioSeleccionado.setText(doc.getNombre());
+        }else if (objetoSeleccionado instanceof Alumno){
+            Alumno alumno = (Alumno)objetoSeleccionado;
+            lbl_usuarioSeleccionado.setText(alumno.getNombre());
+        }else{
+           lbl_usuarioSeleccionado.setText("Dato seleccionado no es un usuario.");
+        }
+        
+    }//GEN-LAST:event_jt_usuariosMouseClicked
 
     /**
      * @param args the command line arguments
@@ -179,9 +252,11 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JComboBox<String> jcb_tipoUsuario;
     private javax.swing.JTree jt_usuarios;
+    private javax.swing.JLabel lbl_usuarioSeleccionado;
     private javax.swing.JTextField txt_nombre;
     // End of variables declaration//GEN-END:variables
 }
