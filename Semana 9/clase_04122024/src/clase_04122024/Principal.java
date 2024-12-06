1bead2ae5cee05aec1f2c74d7071b2bc4f5b5f16
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2024-12-05 18:33:59 -0600

    Clase 5 diciembre: hilos

--- Semana 9/clase_04122024/src/clase_04122024/Principal.java
+++ Semana 9/clase_04122024/src/clase_04122024/Principal.java
@@ -4,6 +4,8 @@
  */
 package clase_04122024;
 
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author claudiacortes
@@ -26,18 +28,22 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lbl_porcentaje = new javax.swing.JLabel();
         btn_iniciar = new javax.swing.JButton();
         btn_pausar = new javax.swing.JButton();
         btn_detener = new javax.swing.JButton();
-        jpb_progreso = new javax.swing.JProgressBar();
         btn_aumentar = new javax.swing.JButton();
         btn_disminuir = new javax.swing.JButton();
         btn_continuar = new javax.swing.JButton();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel1 = new javax.swing.JPanel();
+        lbl_porcentaje = new javax.swing.JLabel();
+        jpb_progreso = new javax.swing.JProgressBar();
+        lbl_etiqueta = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        txt_prueba = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        lbl_porcentaje.setText("jLabel1");
 
         btn_iniciar.setText("iniciar");
         btn_iniciar.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -81,6 +87,70 @@ public class Principal extends javax.swing.JFrame {
             }
         });
 
+        lbl_porcentaje.setText("jLabel1");
+
+        lbl_etiqueta.setText("jLabel1");
+
+        jButton1.setText("actualizar");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+
+        txt_prueba.setText("jTextField2");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(232, 232, 232)
+                        .addComponent(lbl_porcentaje))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(jpb_progreso, javax.swing.GroupLayout.PREFERRED_SIZE, 450, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(67, 67, 67)
+                        .addComponent(lbl_etiqueta)
+                        .addGap(60, 60, 60)
+                        .addComponent(jButton1)
+                        .addGap(60, 60, 60)
+                        .addComponent(txt_prueba, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(69, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(lbl_porcentaje)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jpb_progreso, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbl_etiqueta)
+                    .addComponent(jButton1)
+                    .addComponent(txt_prueba, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(49, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("tab1", jPanel1);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 566, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 192, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("tab2", jPanel2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -89,12 +159,7 @@ public class Principal extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(62, 62, 62)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jpb_progreso, javax.swing.GroupLayout.PREFERRED_SIZE, 450, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btn_iniciar)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(258, 258, 258)
-                        .addComponent(lbl_porcentaje))
+                        .addComponent(btn_iniciar))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(172, 172, 172)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -107,17 +172,16 @@ public class Principal extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(btn_aumentar)
                                 .addGap(70, 70, 70)
-                                .addComponent(btn_disminuir)))))
-                .addContainerGap(94, Short.MAX_VALUE))
+                                .addComponent(btn_disminuir))))
+                    .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 566, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(40, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(55, 55, 55)
-                .addComponent(lbl_porcentaje)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
-                .addComponent(jpb_progreso, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(67, 67, 67)
+                .addContainerGap()
+                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btn_iniciar)
                     .addComponent(btn_pausar)
@@ -156,9 +220,16 @@ public class Principal extends javax.swing.JFrame {
                if(pausa){
                    System.out.println("El hilo esta en pausa");
                }else{
-                 jpb_progreso.setValue(progreso);
-                 lbl_porcentaje.setText(progreso+"%");
-                try {
+                   int temporal = progreso;
+                
+                   // se utiliza para que las actualizaciones de elementos visuales en java se ejecuten sin errores
+                   SwingUtilities.invokeLater(() ->{
+                        jpb_progreso.setValue(temporal);
+                        lbl_porcentaje.setText(temporal+"%");
+                   });
+                   
+
+                try {   
                     Thread.sleep(300); // pone una pequena pausa, en mili segundos
                     progreso++;
                 } catch (Exception e) {
@@ -197,6 +268,13 @@ public class Principal extends javax.swing.JFrame {
         pausa =false;
         detener = false;
     }//GEN-LAST:event_btn_continuarMouseClicked
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        // TODO add your handling code here:
+        
+        
+        lbl_etiqueta.setText(txt_prueba.getText());
+    }//GEN-LAST:event_jButton1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -243,7 +321,13 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JButton btn_disminuir;
     private javax.swing.JButton btn_iniciar;
     private javax.swing.JButton btn_pausar;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JProgressBar jpb_progreso;
+    private javax.swing.JLabel lbl_etiqueta;
     private javax.swing.JLabel lbl_porcentaje;
+    private javax.swing.JTextField txt_prueba;
     // End of variables declaration//GEN-END:variables
 }
